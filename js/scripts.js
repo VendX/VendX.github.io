235433b61c379bd9e0465ce27d6a26a22150e760
Balamurali M <balamurali05@outlook.com>
2020-06-19 11:15:44 +0530

    Oreon rename (#4)
    * ✨ Rename all evolo to oreon
    
    - ✨ Add OGP content
    - 🔥 Comment out footer
    
    * ✨ 🔥 Remove image logo in navbar & update favicon
    
    * 🚧 Start work on email form
    
    * ✨ Make email form work
    
    🔥 Comment out jquery checking of form

--- js/scripts.js
+++ js/scripts.js
@@ -145,205 +145,205 @@
 	});
     
     
-    /* Move Form Fields Label When User Types */
-    // for input and textarea fields
-    $("input, textarea").keyup(function(){
-		if ($(this).val() != '') {
-			$(this).addClass('notEmpty');
-		} else {
-			$(this).removeClass('notEmpty');
-		}
-    });
+    // /* Move Form Fields Label When User Types */
+    // // for input and textarea fields
+    // $("input, textarea").keyup(function(){
+		// if ($(this).val() != '') {
+			// $(this).addClass('notEmpty');
+		// } else {
+			// $(this).removeClass('notEmpty');
+		// }
+    // });
 
 
-    /* Request Form */
-    $("#requestForm").validator().on("submit", function(event) {
-    	if (event.isDefaultPrevented()) {
-            // handle the invalid form...
-            rformError();
-            rsubmitMSG(false, "Please fill all fields!");
-        } else {
-            // everything looks good!
-            event.preventDefault();
-            rsubmitForm();
-        }
-    });
+    // /* Request Form */
+    // $("#requestForm").validator().on("submit", function(event) {
+    // 	if (event.isDefaultPrevented()) {
+    //         // handle the invalid form...
+    //         rformError();
+    //         rsubmitMSG(false, "Please fill all fields!");
+    //     } else {
+    //         // everything looks good!
+    //         event.preventDefault();
+    //         rsubmitForm();
+    //     }
+    // });
 
-    function rsubmitForm() {
-        // initiate variables with form content
-		var name = $("#rname").val();
-		var email = $("#remail").val();
-		var phone = $("#rphone").val();
-        var select = $("#rselect").val();
-        var terms = $("#rterms").val();
+    // function rsubmitForm() {
+    //     // initiate variables with form content
+		// var name = $("#rname").val();
+		// var email = $("#remail").val();
+		// var phone = $("#rphone").val();
+    //     var select = $("#rselect").val();
+    //     var terms = $("#rterms").val();
         
-        $.ajax({
-            type: "POST",
-            url: "php/requestform-process.php",
-            data: "name=" + name + "&email=" + email + "&phone=" + phone + "&select=" + select + "&terms=" + terms, 
-            success: function(text) {
-                if (text == "success") {
-                    rformSuccess();
-                } else {
-                    rformError();
-                    rsubmitMSG(false, text);
-                }
-            }
-        });
-	}
+    //     $.ajax({
+    //         type: "POST",
+    //         url: "php/requestform-process.php",
+    //         data: "name=" + name + "&email=" + email + "&phone=" + phone + "&select=" + select + "&terms=" + terms, 
+    //         success: function(text) {
+    //             if (text == "success") {
+    //                 rformSuccess();
+    //             } else {
+    //                 rformError();
+    //                 rsubmitMSG(false, text);
+    //             }
+    //         }
+    //     });
+	// }
 
-    function rformSuccess() {
-        $("#requestForm")[0].reset();
-        rsubmitMSG(true, "Request Submitted!");
-        $("input").removeClass('notEmpty'); // resets the field label after submission
-    }
+    // function rformSuccess() {
+    //     $("#requestForm")[0].reset();
+    //     rsubmitMSG(true, "Request Submitted!");
+    //     $("input").removeClass('notEmpty'); // resets the field label after submission
+    // }
 
-    function rformError() {
-        $("#requestForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-            $(this).removeClass();
-        });
-	}
+    // function rformError() {
+    //     $("#requestForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+    //         $(this).removeClass();
+    //     });
+	// }
 
-    function rsubmitMSG(valid, msg) {
-        if (valid) {
-            var msgClasses = "h3 text-center tada animated";
-        } else {
-            var msgClasses = "h3 text-center";
-        }
-        $("#rmsgSubmit").removeClass().addClass(msgClasses).text(msg);
-    }
+    // function rsubmitMSG(valid, msg) {
+    //     if (valid) {
+    //         var msgClasses = "h3 text-center tada animated";
+    //     } else {
+    //         var msgClasses = "h3 text-center";
+    //     }
+    //     $("#rmsgSubmit").removeClass().addClass(msgClasses).text(msg);
+    // }
     
 
-    /* Contact Form */
-    $("#contactForm").validator().on("submit", function(event) {
-    	if (event.isDefaultPrevented()) {
-            // handle the invalid form...
-            cformError();
-            csubmitMSG(false, "Please fill all fields!");
-        } else {
-            // everything looks good!
-            event.preventDefault();
-            csubmitForm();
-        }
-    });
+    // /* Contact Form */
+    // $("#contactForm").validator().on("submit", function(event) {
+    // 	if (event.isDefaultPrevented()) {
+    //         // handle the invalid form...
+    //         cformError();
+    //         csubmitMSG(false, "Please fill all fields!");
+    //     } else {
+    //         // everything looks good!
+    //         event.preventDefault();
+    //         csubmitForm();
+    //     }
+    // });
 
-    function csubmitForm() {
-        // initiate variables with form content
-		var name = $("#cname").val();
-		var email = $("#cemail").val();
-        var message = $("#cmessage").val();
-        var terms = $("#cterms").val();
-        $.ajax({
-            type: "POST",
-            url: "php/contactform-process.php",
-            data: "name=" + name + "&email=" + email + "&message=" + message + "&terms=" + terms, 
-            success: function(text) {
-                if (text == "success") {
-                    cformSuccess();
-                } else {
-                    cformError();
-                    csubmitMSG(false, text);
-                }
-            }
-        });
-	}
+    // function csubmitForm() {
+    //     // initiate variables with form content
+		// var name = $("#cname").val();
+		// var email = $("#cemail").val();
+    //     var message = $("#cmessage").val();
+    //     var terms = $("#cterms").val();
+    //     $.ajax({
+    //         type: "POST",
+    //         url: "php/contactform-process.php",
+    //         data: "name=" + name + "&email=" + email + "&message=" + message + "&terms=" + terms, 
+    //         success: function(text) {
+    //             if (text == "success") {
+    //                 cformSuccess();
+    //             } else {
+    //                 cformError();
+    //                 csubmitMSG(false, text);
+    //             }
+    //         }
+    //     });
+	// }
 
-    function cformSuccess() {
-        $("#contactForm")[0].reset();
-        csubmitMSG(true, "Message Submitted!");
-        $("input").removeClass('notEmpty'); // resets the field label after submission
-        $("textarea").removeClass('notEmpty'); // resets the field label after submission
-    }
+    // function cformSuccess() {
+    //     $("#contactForm")[0].reset();
+    //     csubmitMSG(true, "Message Submitted!");
+    //     $("input").removeClass('notEmpty'); // resets the field label after submission
+    //     $("textarea").removeClass('notEmpty'); // resets the field label after submission
+    // }
 
-    function cformError() {
-        $("#contactForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-            $(this).removeClass();
-        });
-	}
+    // function cformError() {
+    //     $("#contactForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+    //         $(this).removeClass();
+    //     });
+	// }
 
-    function csubmitMSG(valid, msg) {
-        if (valid) {
-            var msgClasses = "h3 text-center tada animated";
-        } else {
-            var msgClasses = "h3 text-center";
-        }
-        $("#cmsgSubmit").removeClass().addClass(msgClasses).text(msg);
-    }
+    // function csubmitMSG(valid, msg) {
+    //     if (valid) {
+    //         var msgClasses = "h3 text-center tada animated";
+    //     } else {
+    //         var msgClasses = "h3 text-center";
+    //     }
+    //     $("#cmsgSubmit").removeClass().addClass(msgClasses).text(msg);
+    // }
 
 
-    /* Privacy Form */
-    $("#privacyForm").validator().on("submit", function(event) {
-    	if (event.isDefaultPrevented()) {
-            // handle the invalid form...
-            pformError();
-            psubmitMSG(false, "Please fill all fields!");
-        } else {
-            // everything looks good!
-            event.preventDefault();
-            psubmitForm();
-        }
-    });
+    // /* Privacy Form */
+    // $("#privacyForm").validator().on("submit", function(event) {
+    // 	if (event.isDefaultPrevented()) {
+    //         // handle the invalid form...
+    //         pformError();
+    //         psubmitMSG(false, "Please fill all fields!");
+    //     } else {
+    //         // everything looks good!
+    //         event.preventDefault();
+    //         psubmitForm();
+    //     }
+    // });
 
-    function psubmitForm() {
-        // initiate variables with form content
-		var name = $("#pname").val();
-		var email = $("#pemail").val();
-        var select = $("#pselect").val();
-        var terms = $("#pterms").val();
+    // function psubmitForm() {
+    //     // initiate variables with form content
+		// var name = $("#pname").val();
+		// var email = $("#pemail").val();
+    //     var select = $("#pselect").val();
+    //     var terms = $("#pterms").val();
         
-        $.ajax({
-            type: "POST",
-            url: "php/privacyform-process.php",
-            data: "name=" + name + "&email=" + email + "&select=" + select + "&terms=" + terms, 
-            success: function(text) {
-                if (text == "success") {
-                    pformSuccess();
-                } else {
-                    pformError();
-                    psubmitMSG(false, text);
-                }
-            }
-        });
-	}
+    //     $.ajax({
+    //         type: "POST",
+    //         url: "php/privacyform-process.php",
+    //         data: "name=" + name + "&email=" + email + "&select=" + select + "&terms=" + terms, 
+    //         success: function(text) {
+    //             if (text == "success") {
+    //                 pformSuccess();
+    //             } else {
+    //                 pformError();
+    //                 psubmitMSG(false, text);
+    //             }
+    //         }
+    //     });
+	// }
 
-    function pformSuccess() {
-        $("#privacyForm")[0].reset();
-        psubmitMSG(true, "Request Submitted!");
-        $("input").removeClass('notEmpty'); // resets the field label after submission
-    }
+    // function pformSuccess() {
+    //     $("#privacyForm")[0].reset();
+    //     psubmitMSG(true, "Request Submitted!");
+    //     $("input").removeClass('notEmpty'); // resets the field label after submission
+    // }
 
-    function pformError() {
-        $("#privacyForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-            $(this).removeClass();
-        });
-	}
+    // function pformError() {
+    //     $("#privacyForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+    //         $(this).removeClass();
+    //     });
+	// }
 
-    function psubmitMSG(valid, msg) {
-        if (valid) {
-            var msgClasses = "h3 text-center tada animated";
-        } else {
-            var msgClasses = "h3 text-center";
-        }
-        $("#pmsgSubmit").removeClass().addClass(msgClasses).text(msg);
-    }
+    // function psubmitMSG(valid, msg) {
+    //     if (valid) {
+    //         var msgClasses = "h3 text-center tada animated";
+    //     } else {
+    //         var msgClasses = "h3 text-center";
+    //     }
+    //     $("#pmsgSubmit").removeClass().addClass(msgClasses).text(msg);
+    // }
     
 
-    /* Back To Top Button */
-    // create the back to top button
-    $('body').prepend('<a href="body" class="back-to-top page-scroll">Back to Top</a>');
-    var amountScrolled = 700;
-    $(window).scroll(function() {
-        if ($(window).scrollTop() > amountScrolled) {
-            $('a.back-to-top').fadeIn('500');
-        } else {
-            $('a.back-to-top').fadeOut('500');
-        }
-    });
+    // /* Back To Top Button */
+    // // create the back to top button
+    // $('body').prepend('<a href="body" class="back-to-top page-scroll">Back to Top</a>');
+    // var amountScrolled = 700;
+    // $(window).scroll(function() {
+    //     if ($(window).scrollTop() > amountScrolled) {
+    //         $('a.back-to-top').fadeIn('500');
+    //     } else {
+    //         $('a.back-to-top').fadeOut('500');
+    //     }
+    // });
 
 
-	/* Removes Long Focus On Buttons */
-	$(".button, a, button").mouseup(function() {
-		$(this).blur();
-	});
+	// /* Removes Long Focus On Buttons */
+	// $(".button, a, button").mouseup(function() {
+		// $(this).blur();
+	// });
 
 })(jQuery);
